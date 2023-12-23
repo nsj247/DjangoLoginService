626075a1aafb203d5e0aa2831a2b321c9e315552
sjna <sjna>
2023-12-24 00:02:03 +0900

    remove userprofile model in login app

--- login/models.py
+++ login/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
